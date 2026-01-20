4586878b388ff3047066a02c8e17733b04d681e0
ldupclay27-ship-it <ldupclay27@bfhsla.org>
2026-01-20 15:27:03 -0600

    day 3

---
+++ Scenes/projectile.gd
@@ -0,0 +1,12 @@
+extends Area3D
+
+const SPEED = 20.0
+const RANGE = 40.0
+
+var travelled_distance = 0.0
+
+func _physics_process(delta: float) -> void:
+	position += transform.basis.z * SPEED * delta
+	travelled_distance += SPEED * delta
+	if travelled_distance > RANGE:
+		queue_free()
